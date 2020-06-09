e068ac5f536da8aeacf81331da9cf9b400530c7c
Viktor Tsvetkov <55536977+viktortsvet@users.noreply.github.com>
2020-06-09 20:08:07 +0300

    Add files via upload

---
+++ posts-app/precache-manifest.ed1d3032f2311fa7c8e38db17fcbd14b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74fd074373ce7b470aa96cf6ffff8e82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec01af070ca4771a1f3c",
+    "url": "/static/css/main.959d0be5.chunk.css"
+  },
+  {
+    "revision": "9e82ea31952bb4185364",
+    "url": "/static/js/2.9a752cfa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9a752cfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec01af070ca4771a1f3c",
+    "url": "/static/js/main.1a1eeaf3.chunk.js"
+  },
+  {
+    "revision": "a26f3c5ce9de9e60313b",
+    "url": "/static/js/runtime-main.ab1a1229.js"
+  }
+]);
