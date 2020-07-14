ce74e2c18fb25fb145579703790a19f03047cf32
Viktor Tsvetkov <55536977+viktortsvet@users.noreply.github.com>
2020-07-14 10:35:10 +0300

    Add files via upload

---
+++ matrix/precache-manifest.b822d9b778f20f023be8ff41c7bc836a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f14bdb03ea56744a9cfaa382cb296f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e6218d3c5329c1cd00f8",
+    "url": "/static/css/main.5c4f25ab.chunk.css"
+  },
+  {
+    "revision": "9e083b034dd072f89267",
+    "url": "/static/js/2.8a37d2e8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.8a37d2e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e6218d3c5329c1cd00f8",
+    "url": "/static/js/main.f8483b10.chunk.js"
+  },
+  {
+    "revision": "2408efb9a920f96bf843",
+    "url": "/static/js/runtime-main.cee07cbb.js"
+  }
+]);
